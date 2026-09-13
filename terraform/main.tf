d1fe5a9851cb9a87d7442da4db7e9a36f0301603
jawad <prostyle.tex@gmail.com>
2026-09-13 21:02:12 +0500

    Add OpenROAD Kubernetes job

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,43 @@ terraform {
 provider "kubernetes" {
   config_path = "/var/lib/jenkins/.kube/config"
 }
+
 resource "kubernetes_namespace_v1" "semiconductor_devops" {
   metadata {
     name = "semiconductor-devops-phase2"
+  }
+}
+
+resource "kubernetes_job_v1" "openroad" {
+  metadata {
+    name      = "openroad-eda-job"
+    namespace = kubernetes_namespace_v1.semiconductor_devops.metadata[0].name
+  }
+
+  spec {
+    backoff_limit = 1
+
+    template {
+      metadata {
+        labels = {
+          app = "openroad-eda"
+        }
+      }
+
+      spec {
+        restart_policy = "Never"
+
+        container {
+          name  = "openroad"
+          image = "ghcr.io/the-openroad-project/openlane:1.0.2"
+
+          command = [
+            "/bin/bash",
+            "-c",
+            "/build/bin/openroad -version"
+          ]
+        }
+      }
+    }
   }
 }
